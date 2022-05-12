f97fe2e2260edfbc6c3d8482049c51ff9eccdfd6
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 <yeji262@gmail.com>
2022-05-12 11:10:28 +0900

    reportlist 함

--- app/src/main/java/com/example/citybang/ReportlistActivity.java
+++ app/src/main/java/com/example/citybang/ReportlistActivity.java
@@ -3,6 +3,7 @@ package com.example.citybang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class ReportlistActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class ReportlistActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reportlist);
+
+
+
     }
+
+
+
 }
