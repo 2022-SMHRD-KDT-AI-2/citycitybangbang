39f6451e8289a2e576291e4a7511d094422243c6
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 <yeji262@gmail.com>
2022-05-12 15:54:06 +0900

    충돌

---
+++ app/src/main/java/com/example/citybang/GalaryActivity.java
@@ -0,0 +1,14 @@
+package com.example.citybang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GalaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galary);
+    }
+}
