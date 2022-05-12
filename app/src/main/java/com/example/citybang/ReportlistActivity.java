c1eb67c97e0507c9a022e22e00aae8b569822cee
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 <yeji262@gmail.com>
2022-05-12 15:53:05 +0900

    신고리스트 and 툴바

--- app/src/main/java/com/example/citybang/ReportlistActivity.java
+++ app/src/main/java/com/example/citybang/ReportlistActivity.java
@@ -1,21 +1,38 @@
 package com.example.citybang;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.view.MenuItem;
 
 public class ReportlistActivity extends AppCompatActivity {
 
+    // 툴바
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reportlist);
 
-
-
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);  // 툴바 뒤로가기 누르면 여기로 간다!
+                startActivity(intent);
 
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 
 }
