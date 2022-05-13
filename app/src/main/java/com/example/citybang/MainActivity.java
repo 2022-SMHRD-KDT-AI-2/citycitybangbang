8a1a391746a33585ae33535c621fe785019e078c
jihyeon Kim <zhixian117j@gmail.com>
2022-05-13 17:09:48 +0900

    메인페이지 완성

--- app/src/main/java/com/example/citybang/MainActivity.java
+++ app/src/main/java/com/example/citybang/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.citybang;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -19,11 +21,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.btnDraWithdrawal);
+        button.setPaintFlags(button.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
 
     }
