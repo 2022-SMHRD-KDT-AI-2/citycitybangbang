725700660c871045dd47a8c68b944968b2f9d2d2
jihyeon Kim <zhixian117j@gmail.com>
2022-05-14 14:49:04 +0900

    일자 변경 클릭시 달력 구현

--- app/src/main/java/com/example/citybang/AutoActivity.java
+++ app/src/main/java/com/example/citybang/AutoActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Paint;
@@ -12,10 +13,14 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AutoActivity extends AppCompatActivity {
 
-    Button autoBtnCancel;
+    Button autoBtnCancel,autoBtnArea;
+    TextView autoTvArea;
 
 
     @Override
@@ -58,8 +63,28 @@ public class AutoActivity extends AppCompatActivity {
         });
         // 팝업 창 끝
 
+        // 버튼클릭 시 날짜 다이얼로그 출력
+        autoTvArea = findViewById(R.id.autoTvArea);
+        autoBtnArea = findViewById(R.id.autoBtnArea);
+        autoBtnArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog dialog = new DatePickerDialog(AutoActivity.this, listener, 2022, 5, 13);
+                dialog.show();
+            }
+        });
+        // 버튼클릭 날짜 끝
 
     }
+
+    // 버튼클릭 리스너 생성
+    private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            Toast.makeText(getApplicationContext(), year + "년" + monthOfYear + "월" + dayOfMonth +"일", Toast.LENGTH_SHORT).show();
+            autoTvArea.setText(year + "년" + monthOfYear + "월" + dayOfMonth +"일");
+        }
+    };
 
 
 }
