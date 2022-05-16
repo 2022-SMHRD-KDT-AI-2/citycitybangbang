23b5e742026e6380cc4fa7e69c664f9a293ddbfb
jihyeon Kim <zhixian117j@gmail.com>
2022-05-16 12:49:36 +0900

    위치 지도 xml 중간저장

---
+++ app/src/main/java/com/example/citybang/MapActivity.java
@@ -0,0 +1,36 @@
+package com.example.citybang;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import net.daum.mf.map.api.MapView;
+
+
+
+public class MapActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        // 툴바
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //MapView mapView = new MapView(this);
+        MapView mapView = new MapView(this);
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+
+    }
+}
