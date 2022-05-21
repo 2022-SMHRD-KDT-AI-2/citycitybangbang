0656fd749a6dda8804163f87cc0cb9dc2598e362
jihyeon Kim <zhixian117j@gmail.com>
2022-05-21 12:21:19 +0900

    카메라..

--- app/src/main/java/com/example/citybang/MainActivity.java
+++ app/src/main/java/com/example/citybang/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.citybang;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
@@ -11,6 +13,8 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -21,6 +25,8 @@ import com.android.volley.toolbox.Volley;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_PERMISSION = 11;
 
     Button btnMaiCamera, btnMaiSiren, btnMaiGallery, btnDraReportlist,
             btnDraLaw1, btnDraLaw2, btnDraAsk, btnDraStep, btnDraWithdrawal,
@@ -221,5 +227,64 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // 카메라 이동
+        btnMaiCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                checkPermission(); //권한체크
+
+                intent = new Intent(getApplicationContext(),ShootselectActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        checkPermission(); //권한체크
+    }
+
+    //권한 확인
+    public void checkPermission() {
+        int permissionCamera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int permissionRead = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int permissionWrite = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        //권한이 없으면 권한 요청
+        if (permissionCamera != PackageManager.PERMISSION_GRANTED
+                || permissionRead != PackageManager.PERMISSION_GRANTED
+                || permissionWrite != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                Toast.makeText(this, "이 앱을 실행하기 위해 권한이 필요합니다.", Toast.LENGTH_SHORT).show();
+            }
+
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_PERMISSION: {
+                // 권한이 취소되면 result 배열은 비어있다.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this, "권한 확인", Toast.LENGTH_LONG).show();
+
+                } else {
+                    Toast.makeText(this, "권한 없음", Toast.LENGTH_LONG).show();
+                    finish(); //권한이 없으면 앱 종료
+                }
+            }
+        }
+    }
+
+
 }
