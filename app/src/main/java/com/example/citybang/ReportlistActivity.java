6143b06d58df4b2a5e2171b5d92cf0c2ba4e69f7
wjdgusgh11!! <xman955@naver.com>
2022-05-23 21:54:04 +0900

    내 신고목록

--- app/src/main/java/com/example/citybang/ReportlistActivity.java
+++ app/src/main/java/com/example/citybang/ReportlistActivity.java
@@ -4,12 +4,26 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class ReportlistActivity extends AppCompatActivity {
+
+    RequestQueue requestQueue;
+
+    String a = "";
 
     // 툴바
     @Override
@@ -17,10 +31,16 @@ public class ReportlistActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reportlist);
 
+        String a = SharedPreference.getAttribute(getBaseContext(), "id");
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        if (requestQueue == null){
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
     }
 
     @Override
@@ -34,6 +54,8 @@ public class ReportlistActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 
 
 }
