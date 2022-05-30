7fc07d056cd19b7296f375c9bd5d1c96bdf711f1
wjdgusgh11!! <xman955@naver.com>
2022-05-30 14:33:14 +0900

    ip 및 시간 수정

--- app/src/main/java/com/example/citybang/MainActivity.java
+++ app/src/main/java/com/example/citybang/MainActivity.java
@@ -84,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        String url = "http://59.3.122.222:8081/citycitybangbang/reportNum?";
+        String url = "http://125.136.66.65:8081/citycitybangbang/reportNum?";
         StringRequest request = new StringRequest(
                 Request.Method.GET, url, new Response.Listener<String>(){
             @Override
@@ -154,7 +154,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
 
-                        String url = "http://59.3.122.222:8081/citycitybangbang/deleteUser?id=" + a;
+                        String url = "http://125.136.66.65:8081/citycitybangbang/deleteUser?id=" + a;
                         StringRequest request = new StringRequest(
                                 Request.Method.GET, url, new Response.Listener<String>(){
                             @Override
@@ -243,7 +243,7 @@ public class MainActivity extends AppCompatActivity {
         btnMaiList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String url = "http://59.3.122.222:8081/citycitybangbang/reSuccess?id=" + a;
+                String url = "http://125.136.66.65:8081/citycitybangbang/reSuccess?id=" + a;
                 StringRequest request = new StringRequest(
                         Request.Method.GET, url, new Response.Listener<String>(){
                     @Override
@@ -279,15 +279,8 @@ public class MainActivity extends AppCompatActivity {
         btnMaiSiren.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if(a == null) {
-                    Intent intent1 = new Intent(getApplicationContext(), LoginActivity.class);
-                    startActivity(intent1);
-                }else if(a != null){
-                    intent = new Intent(getApplicationContext(),AutoActivity.class);
-                    startActivity(intent);
-                }
-
+                intent = new Intent(getApplicationContext(),AutoActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -295,7 +288,7 @@ public class MainActivity extends AppCompatActivity {
         btnDraReportlist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String url = "http://59.3.122.222:8081/citycitybangbang/reSuccess?id=" + a;
+                String url = "http://125.136.66.65:8081/citycitybangbang/reSuccess?id=" + a;
                 StringRequest request = new StringRequest(
                         Request.Method.GET, url, new Response.Listener<String>(){
                     @Override
@@ -374,14 +367,9 @@ public class MainActivity extends AppCompatActivity {
 
                 checkPermission(); //권한체크
 
-                if(a == null) {
-                    Intent intent1 = new Intent(getApplicationContext(), LoginActivity.class);
-                    startActivity(intent1);
-                }else if(a != null) {
-                    intent = new Intent(getApplicationContext(), ShootselectActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
+                intent = new Intent(getApplicationContext(),ShootselectActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
 
